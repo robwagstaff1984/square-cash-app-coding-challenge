09b972b23af709fcc02e96e81983294573953b66
Robert Wagstaff <robertwagstaff@Roberts-MacBook-Pro-2.local>
2019-08-17 12:27:28 +1000

    Initialised a new project

---
+++ SquareCashAppCodingChallenge/SquareCashAppCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquareCashAppCodingChallenge
+//
+//  Created by Robert Wagstaff on 17/8/19.
+//  Copyright © 2019 Robert Wagstaff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
